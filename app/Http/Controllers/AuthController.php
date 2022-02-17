e8e3d6d4647f622baff344a58062779822953d16
Ronildo Sousa <sousaronildo400@gmail.com>
2022-02-17 16:05:24 -0300

    teste remove repository

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
-use App\Repositories\AuthRepository;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
@@ -12,13 +11,6 @@ use Inertia\Inertia;
 
 class AuthController extends Controller
 {
-    private $repository;
-
-    public function __construct(AuthRepository $repository)
-    {
-        $this->repository = $repository;
-    }
-
     public function registerForm()
     {
         if (!Auth::user()) {
@@ -35,22 +27,22 @@ class AuthController extends Controller
         return Redirect::route('admin.index');
     }
 
-    public function register(RegisterRequest $request)
-    {
-        $callback = $this->repository->store($request->validated());
-        if ($callback === true) {
-            return Redirect::route('admin.index');
-        }
-        return Redirect::back()->withErrors(['errors'=> 'error to create  user.']);
-    }
+    // public function register(RegisterRequest $request)
+    // {
+    //     $callback = $this->repository->store($request->validated());
+    //     if ($callback === true) {
+    //         return Redirect::route('admin.index');
+    //     }
+    //     return Redirect::back()->withErrors(['errors'=> 'error to create  user.']);
+    // }
 
-    public function login(LoginRequest $request)
-    {
-        if($this->repository->auth($request->validated())){
-            return Redirect::route('admin.index');
-        }
-        return Redirect::back()->withErrors(['credentials'=> 'wrong credentials.']);
-    }
+    // public function login(LoginRequest $request)
+    // {
+    //     if($this->repository->auth($request->validated())){
+    //         return Redirect::route('admin.index');
+    //     }
+    //     return Redirect::back()->withErrors(['credentials'=> 'wrong credentials.']);
+    // }
 
     public function logout()
     {
